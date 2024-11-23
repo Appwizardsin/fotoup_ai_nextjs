7ad29e1e08dbccca8ec0f7677691122b6ccb0b2c
Appwizardsin <151381700+Appwizardsin@users.noreply.github.com>
2024-11-23 18:38:07 +0530

    api url againnnnnnnnnnnnnnnnnnnn

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_BASE_URL = "https://i23wsqntum.us-east-1.awsapprunner.com/api";
-const API_BASE_URL = "http://localhost:3030/api";
+const API_BASE_URL = "https://i23wsqntum.us-east-1.awsapprunner.com/api";
+// const API_BASE_URL = "http://localhost:3030/api";
 
 // Create a function to get the API instance with optional token
 const createAPI = (token) => {
