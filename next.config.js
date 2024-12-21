8537f3afa1a5ccbe8d0c3b8d28d9fe9f896ed38a
Appwizardsin <151381700+Appwizardsin@users.noreply.github.com>
2024-12-21 15:08:52 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     domains: ['aws-app-runner-bucket.s3.ap-south-1.amazonaws.com'],
     remotePatterns: [
       {
